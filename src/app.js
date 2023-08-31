725a7477e98e652a5e8766a2cb9440e6895c8979
Matias311 <120668998+Matias311@users.noreply.github.com>
2023-08-31 21:33:34 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const excuse = document.querySelector("#excuse");
+const body = document.querySelector("body");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateExcuse = () => {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who = ['The dog', 'My grandma', 'His turtle', 'My bird', "My raccoon"];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'the keys', 'the car', 'the computer'];
+  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch'];
+
+  let whoIndex= Math.floor(Math.random()*who.length);
+  let actionIndex= Math.floor(Math.random()*action.length);
+  let whatIndex= Math.floor(Math.random()*what.length);
+  let whenIndex= Math.floor(Math.random()*when.length);
+
+
+  return who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+  
+}
+
+function sum() {
+  excuse.innerHTML = generateExcuse()
+}
+
+body.addEventListener("onload", sum())
