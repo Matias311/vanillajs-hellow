09b7c6969c0a69fc7823c58bcb9df59541d73840
Matias311 <120668998+Matias311@users.noreply.github.com>
2023-08-31 21:35:48 +0000

    Finish

--- src/app.js
+++ src/app.js
@@ -2,24 +2,34 @@ const excuse = document.querySelector("#excuse");
 const body = document.querySelector("body");
 
 let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "My raccoon"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car", "the computer"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+  ];
 
-  let who = ['The dog', 'My grandma', 'His turtle', 'My bird', "My raccoon"];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'the keys', 'the car', 'the computer'];
-  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch'];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
 
-  let whoIndex= Math.floor(Math.random()*who.length);
-  let actionIndex= Math.floor(Math.random()*action.length);
-  let whatIndex= Math.floor(Math.random()*what.length);
-  let whenIndex= Math.floor(Math.random()*when.length);
-
-
-  return who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex] + " " + when[whenIndex];
-  
-}
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+};
 
 function sum() {
-  excuse.innerHTML = generateExcuse()
+  excuse.innerHTML = generateExcuse();
 }
 
-body.addEventListener("onload", sum())
+body.addEventListener("onload", sum());
